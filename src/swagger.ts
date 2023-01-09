3178f94374b1e64127907d86b3126535133074bd
emmabraboke <emmabraboke@gmail.com>
2023-01-09 10:15:11 +0100

    editted swagger file

--- src/swagger.ts
+++ src/swagger.ts
@@ -6,8 +6,7 @@ export const swagger = (app: INestApplication) => {
     .addBearerAuth()
     .setTitle('Meal Management API')
     .setDescription(
-      `Meal management application allows users to create, read, update, and delete
-    meal addons.It also allow users to create categories for these addons.`,
+      '#### Meal management application allows users to create, read, update, and delete meal addons.It also allow users to create categories for these addons \n #### Application Base URL- [https://meal-management.onrender.com](https://meal-management.onrender.com)',
     )
     .setVersion('1.0')
     .build();
