da9942ba66bb68e1314dfdb3ea37847ea214f501
emmabraboke <emmabraboke@gmail.com>
2023-01-09 13:47:39 +0100

    fix: set swagger base path

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,6 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('app', () => ({
   port: Number(process.env.PORT) || 3000,
   secret: process.env.JWT_SECRET,
+  swaggerBasePath: process.env.SWAGGER_BASE_PATH,
   accessTokenExpiresIn: Number(process.env.JWT_EXPIRES_IN),
   refreshTokenExpiresIn: 30 * Number(process.env.JWT_EXPIRES_IN),
   connection: process.env.DATABASE_URL || {
